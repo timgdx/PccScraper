05dcfd6db9d62ca37bb74da7ab3f83db86aa8f49
gdx <gdx>
2023-02-04 23:23:01 +0000

    ADD availability option

--- pccscraper.py
+++ pccscraper.py
@@ -11,6 +11,7 @@ import datetime
 MAX_PRICE = 620
 TARGET_PRODUCTS = ["3080","3070 Ti","6800"]
 OPTIMAL_PRICE = [550,400,550]
+AVAILABLE_ONLY = False
 LOOP = 60*5 # 0 - run once; > 0 - run every X seconds
 URL = "https://www.pccomponentes.pt/"
 TARGET_URL = "https://www.pccomponentes.pt/api-v1/products/search?categoryId=2194165b-70a8-4e4e-ab74-0007a55b73ab&sort=price_asc&channel=pt&page={0}&pageSize=40&seller_type[]=pccomponentes_seller&offer_promotional_price_from=200&offer_promotional_price_to={1}"
@@ -57,7 +58,8 @@ def main():
                 break
             productsFound += len(data["articles"])
             for product in data["articles"]:
-                if len(product["availability"]) > 0 and any(x in product["name"] for x in TARGET_PRODUCTS):
+                available = len(product["availability"]) > 0
+                if any(x in product["name"] for x in TARGET_PRODUCTS) and ((AVAILABLE_ONLY and available) or not AVAILABLE_ONLY):
                     price = float(product["originalPrice"]) if not (product["promotionalPrice"]) else float(product["promotionalPrice"])
                     historyPrice = history.get(product["name"])
                     # save to history
@@ -78,11 +80,15 @@ def main():
                         elif priceDelta < 0:
                             priceDeltaStr = LESS_COLOR+f"({priceDelta:.2f})"+RESET_COLOR
                     #
+                    availabilityColor = RESET_COLOR if available else MORE_COLOR
                     if [price-OPTIMAL_PRICE[x] for x in range(len(TARGET_PRODUCTS)) if TARGET_PRODUCTS[x] in product["name"]][0] > 0:
-                        products.append([priceDeltaStr+str(price),product["name"],link(URL+product["slug"],"Link")])
+                        products.append([priceDeltaStr+str(price),availabilityColor+product["name"]+RESET_COLOR,link(URL+product["slug"],"Link")])
                     else:
-                        products.append([priceDeltaStr+OPTIMAL_COLOR+str(price)+RESET_COLOR,OPTIMAL_COLOR+product["name"]+RESET_COLOR,OPTIMAL_COLOR+link(URL+product["slug"],"Link")+RESET_COLOR])
-                        print('\007') # sound alert
+                        if available:
+                            products.append([priceDeltaStr+OPTIMAL_COLOR+str(price)+RESET_COLOR,OPTIMAL_COLOR+product["name"]+RESET_COLOR,OPTIMAL_COLOR+link(URL+product["slug"],"Link")+RESET_COLOR])
+                            print('\007') # sound alert
+                        else:
+                            products.append([priceDeltaStr+OPTIMAL_COLOR+str(price)+RESET_COLOR,availabilityColor+product["name"]+RESET_COLOR,OPTIMAL_COLOR+link(URL+product["slug"],"Link")+RESET_COLOR])
             currentPage += 1
         print(f"Filtered through {productsFound} products")
         for p in products:
@@ -103,12 +109,11 @@ def exitGracefully(x,y):
     exit()
 
 # https://stackoverflow.com/a/71309268
-def link(uri, label=None): # doesn't work with cmd
+def link(url, label=None): # doesn't work with cmd
     if label is None: 
-        label = uri
-    parameters = ''
+        label = url
     # OSC 8 ; params ; URI ST <name> OSC 8 ;; ST 
-    escape_mask = '\033]8;{};{}\033\\{}\033]8;;\033\\'
-    return escape_mask.format(parameters, uri, label)
-
+    #escape_mask = '\033]8;{};{}\033\\{}\033]8;;\033\\'
+    #return escape_mask.format(parameters, uri, label)
+    return f"\x1b]8;;{url}\a{label}\x1b]8;;\a"
 main()
